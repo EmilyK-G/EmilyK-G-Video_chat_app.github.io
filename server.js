ca57359e65dc41767aec9e04e6568a44b01be12f
Emily.Garcia <emilikimberly1@gmail.com>
2021-07-26 18:26:05 -0400

    renamed webRTC.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://EmilyKG:Password@cluster0.9u4ps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect('mongodb+srv://EmilyKG:Password@cluster0.9u4ps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 
 app.use("/", require("./routes/meetingRoute"));
 
